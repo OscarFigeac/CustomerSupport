631049d36b341d3e9b346fcbcb876127b7fee8d7
TomaszJanuszkiewicz <janutomasz@gmail.com>
2025-04-10 10:43:09 +0100

    Feature: Created the user class with its constructor, getters and setters (No default empty constructor and validation for null inputs is implemented)

--- src/main/java/utils/User.java
+++ src/main/java/utils/User.java
@@ -1,5 +1,56 @@
 package utils;
 
 public class User {
+    private String username;
+    private String password;
 
+    public User(String username, String password) {
+        validateUsername(username);
+        validateUsername(password);
+
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        validateUsername(username);
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        validatePassword(password);
+        this.password = password;
+    }
+
+    /**
+     * Validates username input from user
+     * @param username The value to be validated
+     * @throws IllegalArgumentException If the parameter entered is null
+     * @author Tomasz Januszkiewicz
+     */
+    public void validateUsername(String username){
+        if(username == null){
+            throw new IllegalArgumentException("Parameter cannot be null");
+        }
+    }
+
+    /**
+     * Validates password input from user
+     * @param password The value to be validated
+     * @throws IllegalArgumentException If the parameter entered is null
+     * @author Tomasz Januszkiewicz
+     */
+    public void validatePassword(String password){
+        if(password == null){
+            throw new IllegalArgumentException("Parameter cannot be null");
+        }
+    }
 }
