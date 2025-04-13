de662c0c7be9fc0856110b00dea68ca7f42a13cd
EoghanJPC <eoghanjpc@gmail.com>
2025-04-13 23:34:06 +0100

    Tickets and Attribute Creation

---
+++ src/main/java/utils/Tickets.java
@@ -0,0 +1,22 @@
+package utils;
+
+import java.time.LocalDateTime;
+
+public class Tickets {
+    //Attributes
+    private String ticketId;
+    private String issueDesc;
+    private int priorityLvl;
+    private LocalDateTime creation;
+    private String username;
+    private String agentId;
+    private String status;
+
+    //Constructors
+
+    //Getters
+
+    //Setters
+
+    //equals()
+}
