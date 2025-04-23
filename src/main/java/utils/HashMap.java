f3b151041f0be9c90be8f4edc3cbeea44659279d
TomaszJanuszkiewicz <janutomasz@gmail.com>
2025-04-23 23:13:36 +0100

    Feature: Created the put method which adds an entry into the map (STILL USING ARRAYLISTS)

--- src/main/java/utils/HashMap.java
+++ src/main/java/utils/HashMap.java
@@ -23,4 +23,53 @@ public class HashMap {
             this.value = value;
         }
     }
+
+    /**
+     * Enters a value into the hashmap
+     * @param key The username of the user/agent
+     * @param value
+     * @return The key if it was present or null if nothing was found matching the key
+     * @throws IllegalArgumentException if the key is null
+     */
+    public Integer put(String key, Integer value){
+        validateKey(key);
+
+        int destinationSlot = calculateSlot(key);
+
+        if(map[destinationSlot] == null){
+            map[destinationSlot] = new ArrayList<Entry>();
+        }
+
+        ArrayList<Entry> slotList = map[destinationSlot];
+        for (int i = 0; i < slotList.size(); i++) {
+            Entry currentEntry = slotList.get(i);
+            if(currentEntry.key.equals(key)){
+                Integer oldValue = currentEntry.value;
+                currentEntry.value = value;
+                return oldValue;
+            }
+        }
+
+        Entry newEntry = new Entry(key, value);
+        slotList.add(newEntry);
+        count++;
+
+        return null;
+    }
+
+    private int calculateSlot(String key) {
+        // Convert key's data into number
+        int hashCode = key.hashCode();
+        // Get positive version of hashCode (in case it was a negative value)
+        hashCode = Math.abs(hashCode);
+        // Convert hashCode into a value within the range of slots for this map
+        int destinationSlot = hashCode % map.length;
+        return destinationSlot;
+    }
+
+    private static void validateKey(String key) {
+        if(key == null){
+            throw new IllegalArgumentException("Key cannot be null");
+        }
+    }
 }
