dcadd33903c4d6d8bcb0df2dec8b7f868643171b
EoghanJPC <eoghanjpc@gmail.com>
2025-04-24 16:39:32 +0100

    Update: DynamicArray creation
    New Dynamic Array added to utils folder with constructors

---
+++ src/main/java/utils/DynamicArray.java
@@ -0,0 +1,22 @@
+package utils;
+
+public class DynamicArray {
+    private final int RESIZE_FACTOR = 2;
+
+    private int numElements;
+    private String[] data;
+
+    //Constructors
+    public DynamicArray(){
+        this(10);
+    }
+    public DynamicArray(int orgSize){
+        if(orgSize <= 0){
+            throw new IllegalArgumentException("The array size is too small");
+        }
+
+        data = new String[orgSize];
+    }
+
+
+}
